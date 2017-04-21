431d60fc4bad344c31d6e6c9cbfb458ee8013bfd
s-matyukevich <s.matyukevich@gmail.com>
2017-04-21 17:03:25 -0600

    authenticated with bosh

--- ci/tasks/backup.sh
+++ ci/tasks/backup.sh
@@ -4,6 +4,6 @@ echo "$BOSH_CACERT" > rootCA.pem
 
 bosh -u $BOSH_USER -p $BOSH_PASSWORD --ca-cert rootCA.pem target $BOSH_URL
 
-bosh backup "bosh-$(date +%Y-%m-%dT%H:%M:%S%z).tgz"
+bosh -u $BOSH_USER -p $BOSH_PASSWORD --ca-cert backup "bosh-$(date +%Y-%m-%dT%H:%M:%S%z).tgz"
 
 echo $STORE_PASSWORD | scp bosh*.tgz $STORE_USER@$STORE_HOST:$STORE_PATH
