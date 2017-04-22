c9e60608e8f9e17c578e985f67b0c7d78b72fc75
s-matyukevich <s.matyukevich@gmail.com>
2017-04-21 18:24:21 -0600

    password changed

--- ci/tasks/backup.sh
+++ ci/tasks/backup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
+apt-get install -y sshpass
+
 echo "$BOSH_CACERT" > rootCA.pem
 
 echo -e "$BOSH_USER\n$BOSH_PASSWORD\n"| bosh --ca-cert rootCA.pem target  $BOSH_URL
 
 bosh backup "bosh-$(date +%Y-%m-%dT%H:%M:%S%z).tgz"
 
-echo -e "yes\n$STORE_PASSWORD" | scp ./bosh*.tgz $STORE_USER@$STORE_HOST:$STORE_PATH
+sshpass -p "$STORE_PASSWORD" scp -oStrictHostKeyChecking=no  ./bosh*.tgz $STORE_USER@$STORE_HOST:$STORE_PATH
