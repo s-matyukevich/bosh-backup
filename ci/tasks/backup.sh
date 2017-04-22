aeb08acd9663c3d98f1bf0f4452cd8e5930fc654
s-matyukevich <s.matyukevich@gmail.com>
2017-04-21 18:16:26 -0600

    scp ars changed

--- ci/tasks/backup.sh
+++ ci/tasks/backup.sh
@@ -6,4 +6,4 @@ echo -e "$BOSH_USER\n$BOSH_PASSWORD\n"| bosh --ca-cert rootCA.pem target  $BOSH_
 
 bosh backup "bosh-$(date +%Y-%m-%dT%H:%M:%S%z).tgz"
 
-echo $STORE_PASSWORD | scp bosh*.tgz $STORE_USER@$STORE_HOST:$STORE_PATH
+echo -e "yes\n$STORE_PASSWORD" | scp bosh*.tgz $STORE_USER@$STORE_HOST:$STORE_PATH
